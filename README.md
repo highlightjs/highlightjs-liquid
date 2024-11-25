db378d5c80cd484587fbe0cc7bd1d1ae621109c8
Laurel Thorburn <codinglaurel@gmail.com>
2024-11-25 11:45:38 -0700

    set up liquid syntax highlight for highlight js

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# liquid
-creating liquid language repo for highlightjs
+# Liquid Language Grammar for highlight.js
+
+![license](https://badgen.net/badge/license/MIT/blue)
+
+A language grammar for Shopify's Liquid templating language in highlight.js.
+
+## Usage
+
+Simply include the highlight.js library in your webpage or Node app, then load this module.
+
+### Static website or simple usage
+
+Simply load the module after loading Highlight.js. You'll use the minified version found in the `dist` directory.
+
+```html
+<script type="text/javascript" src="/path/to/highlight.min.js"></script>
+<script type="text/javascript" src="/path/to/liquid.min.js"></script>
+<script type="text/javascript">
+  hljs.highlightAll();
+</script>
+```
+
+### Using directly from the UNPKG CDN
+
+```html
+<script
+  type="text/javascript"
+  src="https://unpkg.com/highlightjs-liquid@0.9.1/dist/liquid.min.js"
+></script>
+```
+
+- More info: <https://unpkg.com>
+
+### With Node or another build system
+
+If you're using Node / Webpack / Rollup / Browserify, etc, simply require the language module, then register it with Highlight.js.
+
+```javascript
+var hljs = require("highlight.js");
+var hljsLiquid = require("highlightjs-liquid");
+
+hljs.registerLanguage("liquid", hljsLiquid);
+hljs.highlightAll();
+```
+
+## License
+
+This package is released under the MIT License. See [LICENSE](LICENSE) file for details.
+
+### Author & Maintainer
+
+Laurel King <laurel.king@shopify.com>
+
+## Links
+
+- The official site for the Highlight.js library is <https://highlightjs.org/>.
+- The Highlight.js GitHub project: <https://github.com/highlightjs/highlight.js>
+- Learn more about Liquid: <https://shopify.dev/docs/api/liquid>
---
+++ src/liquid.js
@@ -0,0 +1,157 @@
+/*
+Language: Liquid
+Author: Laurel King <laurel.king@shopify.com>
+Contributors: N/A
+Description: Shopify's Liquid templating language
+Website: https://shopify.dev/docs/api/liquid
+*/
+
+const IDENT_RE = '[A-Za-z$_][0-9A-Za-z$_]*';
+
+// Liquid Tags
+const KEYWORDS = [
+  "as", "assign", "break", "capture", "case", "comment", "continue", "cycle",
+  "decrement", "echo", "else", "elsif", "endcapture", "endcase", "endcomment",
+  "endfor", "endform", "endif", "endjavascript", "endraw", "endschema",
+  "endsection", "endstylesheet", "endtablerow", "endunless", "for", "form",
+  "if", "ifchanged", "include", "increment", "javascript", "layout", "liquid",
+  "raw", "render", "schema", "section", "style", "stylesheet", "tablerow",
+  "unless", "when", "with"
+];
+
+const KEYWORDS_SUBSET = [
+    "limit", "offset", "reversed", "range",
+];
+
+// Booleans
+const LITERALS = [
+  "true",
+  "false",
+  "null"
+];
+
+// Objects and Variables
+const BUILT_INS = [
+    "additional_checkout_buttons", "address", "all_country_option_tags",
+    "all_products", "article", "articles", "block", "blog", "blogs",
+    "canonical_url", "cart", "checkout", "collection", "collections",
+    "comment", "content_for_additional_checkout_buttons", "content_for_header",
+    "content_for_index", "content_for_layout", "country_option_tags",
+    "currency", "current_page", "current_tags", "customer", "customer_address",
+    "discount_allocation", "discount_application", "external_video", "font",
+    "forloop", "form", "fulfillment", "gift_card", "handle", "image",
+    "images", "line_item", "link", "linklist", "linklists", "location",
+    "localization", "metafield", "model", "model_source", "order", "page",
+    "page_description", "page_image", "page_title", "pages", "paginate",
+    "part", "policy", "powered_by_link", "predictive_search", "product",
+    "product_option", "product_variant", "recommendations", "request",
+    "routes", "script", "scripts", "search", "section", "selling_plan",
+    "selling_plan_allocation", "selling_plan_group", "settings",
+    "shipping_method", "shop", "shop_locale", "store_availability",
+    "tablerow", "tax_line", "template", "theme", "transaction",
+    "unit_price_measurement", "variant", "video", "video_source",
+    "item", "numbers", "i", "index", "value", "letters", "products"
+  ];
+
+// Liquid Filters
+const FILTERS = [
+    "abs", "append", "at_least", "at_most", "capitalize", "ceil", "compact",
+    "concat", "date", "default", "divided_by", "downcase", "escape",
+    "escape_once", "first", "floor", "join", "last", "lstrip", "map",
+    "minus", "modulo", "newline_to_br", "plus", "prepend", "remove",
+    "remove_first", "replace", "replace_first", "reverse", "round",
+    "rstrip", "size", "slice", "sort", "sort_natural", "split", "strip",
+    "strip_html", "strip_newlines", "times", "truncate", "truncatewords",
+    "uniq", "upcase", "url_decode", "url_encode"
+];
+
+const OPERATORS = [
+    "==", "=", "\\:", "\\.", "\\|", "!=", "<>", ">", "<", ">=", "<=", "contains", "and", "or", "\\[", "\\]"
+];
+
+export default function(hljs) {
+  return {
+    name: 'Liquid',
+    aliases: ['shopify'],
+    case_insensitive: true,
+    // Sublanguage support if you want to embed HTML inside Liquid (optional)
+    subLanguage: 'xml',
+    contains: [
+      // Handle comments in Liquid
+      hljs.COMMENT('{% comment %}', '{% endcomment %}'),
+      hljs.COMMENT('{% raw %}', '{% endraw %}'),
+
+      // Parsing Liquid Tags
+      {
+        className: 'template-tag',
+        begin: '{%-?\\s*',
+        end: '-?%}',
+        keywords: KEYWORDS.join(' '),
+        contains: [
+          {
+            // Capture as sublanguage
+            beginKeywords: KEYWORDS.join(' '),
+            relevance: 10,
+          },
+          // Strings handling
+          hljs.QUOTE_STRING_MODE,
+          // Numbers, booleans and null
+          hljs.C_NUMBER_MODE,
+          {
+            className: 'literal',
+            begin: '\\b(true|false|null)\\b'
+          },
+          // Operators & punctuation
+          {
+            className: 'operator',
+            begin: '(' + OPERATORS.join('|') + ')'
+          },
+          // Built-ins
+          {
+            className: 'built_in',
+            begin: '\\b(' + BUILT_INS.join('|') + ')\\b'
+          },
+          // Filters as function calls
+          {
+            className: 'function',
+            begin: '\\|\\s*(' + FILTERS.join('|') + ')\\b(?:\\s*:\\s*[^\\s,|}]+)*',
+            keywords: FILTERS.join(' ')
+          }
+        ]
+      },
+
+      // Parsing Output tags (`{{` `}}`)
+      {
+        className: 'template-variable',
+        begin: '{{-?',
+        end: '-?}}',
+        contains: [
+          hljs.QUOTE_STRING_MODE,
+          hljs.C_NUMBER_MODE,
+          {
+            className: 'literal',
+            begin: '\\b(true|false|null)\\b'
+          },
+          {
+            className: 'operator',
+            begin: '(' + OPERATORS.join('|') + ')'
+          },
+          {
+            className: 'built_in',
+            begin: '\\b(' + BUILT_INS.join('|') + ')\\b'
+          },
+          {
+            className: 'function',
+            begin: '\\|\\s*(' + FILTERS.join('|') + ')\\b(?:\\s*:\\s*[^\\s,|}]+)*',
+            keywords: FILTERS.join(' ')
+          }
+        ]
+      },
+
+      {
+        className: 'property',
+        begin: '\\b[a-zA-Z0-9_]+\\.([a-zA-Z0-9_]+)\\b'
+      }
+    ]
+  };
+}
