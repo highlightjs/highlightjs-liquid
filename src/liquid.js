42a430d300f50a05138a39a18505ca6911c8550b
Laurel Thorburn <codinglaurel@gmail.com>
2024-11-25 13:14:47 -0700

    add newest liquid inline comments, filters, keywords, and built-ins

--- src/liquid.js
+++ src/liquid.js
@@ -10,17 +10,20 @@ const IDENT_RE = '[A-Za-z$_][0-9A-Za-z$_]*';
 
 // Liquid Tags
 const KEYWORDS = [
-  "as", "assign", "break", "capture", "case", "comment", "continue", "cycle",
+  "as", "assign", "break", "capture", "case", "color", "comment", "continue", "cycle",
   "decrement", "echo", "else", "elsif", "endcapture", "endcase", "endcomment",
   "endfor", "endform", "endif", "endjavascript", "endraw", "endschema",
-  "endsection", "endstylesheet", "endtablerow", "endunless", "for", "form",
-  "if", "ifchanged", "include", "increment", "javascript", "layout", "liquid",
-  "raw", "render", "schema", "section", "style", "stylesheet", "tablerow",
-  "unless", "when", "with"
+  "endsection", "endstylesheet", "endtablerow", "endunless", "font", "for",
+  "for-render", "form", "if", "ifchanged", "include", "increment", "javascript",
+  "layout", "liquid", "paginate", "raw", "react", "render", "schema", "section",
+  "sections", "style", "style-tag", "stylesheet", "tablerow", "unless", "when", "with"
 ];
 
 const KEYWORDS_SUBSET = [
-    "limit", "offset", "reversed", "range",
+    "limit",
+    "offset",
+    "range",
+    "reversed"
 ];
 
 // Booleans
@@ -33,36 +36,41 @@ const LITERALS = [
 // Objects and Variables
 const BUILT_INS = [
     "additional_checkout_buttons", "address", "all_country_option_tags",
-    "all_products", "article", "articles", "block", "blog", "blogs",
-    "canonical_url", "cart", "checkout", "collection", "collections",
-    "comment", "content_for_additional_checkout_buttons", "content_for_header",
+    "all_products", "app", "app_settings", "article", "articles", "block",
+    "block_order", "blocks", "blog", "blogs", "canonical_url", "cart",
+    "checkout", "collection", "collections", "color_scheme", "comment",
+    "content_for_additional_checkout_buttons", "content_for_header",
     "content_for_index", "content_for_layout", "country_option_tags",
     "currency", "current_page", "current_tags", "customer", "customer_address",
     "discount_allocation", "discount_application", "external_video", "font",
-    "forloop", "form", "fulfillment", "gift_card", "handle", "image",
-    "images", "line_item", "link", "linklist", "linklists", "location",
-    "localization", "metafield", "model", "model_source", "order", "page",
-    "page_description", "page_image", "page_title", "pages", "paginate",
-    "part", "policy", "powered_by_link", "predictive_search", "product",
-    "product_option", "product_variant", "recommendations", "request",
-    "routes", "script", "scripts", "search", "section", "selling_plan",
-    "selling_plan_allocation", "selling_plan_group", "settings",
-    "shipping_method", "shop", "shop_locale", "store_availability",
-    "tablerow", "tax_line", "template", "theme", "transaction",
-    "unit_price_measurement", "variant", "video", "video_source",
-    "item", "numbers", "i", "index", "value", "letters", "products"
-  ];
+    "forloop", "form", "fulfillment", "gift_card", "global_block", "handle",
+    "i", "image", "images", "index", "item", "letters", "line_item", "link",
+    "linklist", "linklists", "location", "localization", "media", "metafield",
+    "model", "model_source", "numbers", "order", "page", "page_description",
+    "page_image", "page_title", "pages", "paginate", "part", "policy",
+    "powered_by_link", "predictive_search", "predictive_search_autocomplete",
+    "product", "product_option", "product_variant", "products",
+    "recommendations", "request", "routes", "script", "scripts", "search",
+    "section", "section_blocks", "selling_plan", "selling_plan_allocation",
+    "selling_plan_group", "settings", "shipping_method", "shipping_rates",
+    "shop", "shop_locale", "store_availability", "tablerow", "tax_line",
+    "template", "theme", "transaction", "unit_price_measurement", "value",
+    "variant", "video", "video_source"
+];
 
 // Liquid Filters
 const FILTERS = [
-    "abs", "append", "at_least", "at_most", "capitalize", "ceil", "compact",
-    "concat", "date", "default", "divided_by", "downcase", "escape",
-    "escape_once", "first", "floor", "join", "last", "lstrip", "map",
-    "minus", "modulo", "newline_to_br", "plus", "prepend", "remove",
-    "remove_first", "replace", "replace_first", "reverse", "round",
-    "rstrip", "size", "slice", "sort", "sort_natural", "split", "strip",
-    "strip_html", "strip_newlines", "times", "truncate", "truncatewords",
-    "uniq", "upcase", "url_decode", "url_encode"
+    "abs", "append", "at_least", "at_most", "capitalize", "ceil", "color_brightness",
+    "color_darken", "color_desaturate", "color_extract", "color_lighten",
+    "color_modify", "color_saturate", "color_to_rgb", "compact", "concat",
+    "date", "default", "divided_by", "downcase", "escape", "escape_once",
+    "first", "floor", "font_face", "font_modify", "highlight", "highlight_active",
+    "join", "last", "lstrip", "map", "minus", "modulo", "newline_to_br",
+    "payment_type_img_url", "plus", "prepend", "remove", "remove_first",
+    "replace", "replace_first", "reverse", "round", "rstrip", "size", "slice",
+    "sort", "sort_natural", "split", "strip", "strip_html", "strip_newlines",
+    "time_tag", "times", "truncate", "truncatewords", "uniq", "upcase",
+    "url_decode", "url_encode", "where"
 ];
 
 const OPERATORS = [
@@ -86,6 +94,11 @@ export default function(hljs) {
         end: '\\s*-?%}',
         keywords: KEYWORDS.join(' '),
         contains: [
+          {
+            className: 'comment',
+            begin: '#.*?(?=%})',
+            relevance: 10
+          },
           {
             // Capture as sublanguage
             beginKeywords: KEYWORDS.join(' '),
